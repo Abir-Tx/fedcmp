fef4e324866d0d0b0d5dda4d73c5301826ed48a1
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-04-08 14:54:50 +0600

    Create a logger module

---
+++ fashionMnist/system/utils/logger.py
@@ -0,0 +1,18 @@
+import logging
+import datetime
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    log_file = f"{name}_{timestamp}.log"
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    return logger
