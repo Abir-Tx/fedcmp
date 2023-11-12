71ef44446cb150fecd3afc5ea36182ee3c9d94e6
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-11-12 18:57:36 +0600

    Delete the empty file

--- setupDevEnv.sh
+++
@@ -1,8 +0,0 @@
-# An script to simplify the developement process of the project by automatically installing the dependecies, creating required directories and others.
-# This script is only for Linux based Operating systems
-# Scripted By: Mushfiqur Rahman Abir
-# Date: 30th June, 2023
-# Project: FedCmp
-
-
-
